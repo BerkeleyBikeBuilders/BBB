d76ff1cb0bd6d60370834039b4f0989fc4055e6a
Kevin Ying <yingk@berkeley.edu>
2024-03-02 16:10:41 -0800

    added ID# and compareTo method

--- Full_Sus_Telemetry/Potentiometer.h
+++ Full_Sus_Telemetry/Potentiometer.h
@@ -7,16 +7,17 @@
 
 
 class LPOT {
-  public:
 
     int pin;
     int maxVoltage = 4095;
     int zeroVoltage = 0;
-    float length; //the total length of touch surface in mm
+    float length; // the total length of touch surface in mm
     float V_per_mm;
+    int id; // to uniquely identify the potentiometer
 
+  public:
 
-    void create(int P, float L, int zeroVoltage = 0, int maxVoltage = 4095) {
+    void create(int P, float L, int ZEROVOLTAGE = 0, int MAXVOLTAGE = 4095, int resistanceID = 0) {
       /**
       DESCRIPTION:
       Stores the state in the LPOT linear potentiometer instance.
@@ -26,14 +27,17 @@ class LPOT {
       'L': length of the potentiometer (in mm)
       'zeroVoltage': (Optional) the voltage when the potentiometer is at minimum length
       'maxVoltage': (Optional) the voltage when the potentiometer is at max length
+      'resistanceID': (Optional) the code to uniquely identify the potentiometer based on the voltage value from the ID pin
       */
 
       pin = P;
       pinMode(pin, INPUT);
 
+      id = resistanceID;
+
       length = L;
-      maxVoltage = maxVoltage;
-      zeroVoltage = zeroVoltage;
+      maxVoltage = MAXVOLTAGE;
+      zeroVoltage = ZEROVOLTAGE;
 
       V_per_mm = (maxVoltage - zeroVoltage) / length;
     }
@@ -56,6 +60,14 @@ class LPOT {
       */
 
       zeroVoltage = analogRead(pin);
+    }
+
+    int compareTo(LPOT linPot) {
+      /**
+      DESCRIPTION:
+      returns negative if it's smaller, returns positive if it's larger, returns 0 if they're identical.
+      */
+      return id - linPot.id;
     }
 };
 
